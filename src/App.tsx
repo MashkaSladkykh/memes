6c82540588619a8cdea4bc5396ed713194709183
MashkaSladkykh <masha.sladky@gmail.com>
2025-04-22 15:43:06 +0300

    edited navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { useEffect, useState } from "react";
 import { getMemes, Meme } from "./api/getMemes";
 
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import TablePage from "@/pages/table";
+import CardsPage from "@/pages/cards";
 
 function App() {
   const [data, setData] = useState<Meme[]>();
@@ -29,10 +27,8 @@ function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
+      <Route element={<TablePage />} path="/table" />
+      <Route element={<CardsPage />} path="/cards" />
     </Routes>
   );
 }
